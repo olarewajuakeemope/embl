bb5e1bdcfa982b37010ae0e4f639a3776d3461db
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-11-26 19:48:25 +0100

    Move chart render to renderChart method

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -12,7 +12,15 @@ require('highcharts/modules/exporting')(Highcharts);
 
 class Chart extends Component {
   componentDidMount() {
-    const { publications, query } = this.props;
+    this.renderChart(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.renderChart(nextProps);
+  }
+
+  renderChart = (props) => {
+    const { publications, query } = props;
     const { term, startDate, endDate } = query;
 
     const chartData = publications.map((item) => {
@@ -86,8 +94,8 @@ class Chart extends Component {
       },
       subtitle: {
         text: `${term.toUpperCase()} Search: From
-         ${startDate.toDateString()} 
-         To ${endDate.toDateString()}`,
+          ${startDate.toDateString()} 
+          To ${endDate.toDateString()}`,
       },
       xAxis: {
         categories: xAxisCategories,
@@ -110,122 +118,6 @@ class Chart extends Component {
 
           '<tr><td style="color:white;padding:0">Source: </td>' +
           '<td style="padding:0;color:#999999;"><b>{point.source}</b></td></tr>' +
-
-          '<tr><td style="color:white;padding:0">First Published: </td>' +
-          '<td style="padding:0"><b>{point.firstPublicationDate}</b></td></tr>' +
-
-          '<tr><td style="color:white;padding:0">Citations: </td>' +
-          '<td style="padding:0;color:#999999;"><b>{point.max}</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true,
-      },
-      plotOptions: {
-        column: {
-          pointPadding: 0.2,
-          borderWidth: 0,
-        },
-      },
-      series: [{
-        name: 'Years',
-        data: yAxisCategories,
-      }],
-    });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { publications, query } = nextProps;
-    const { term, startDate, endDate } = query;
-
-    const chartData = publications.map((item) => {
-      const { hitCount, resultList } = item;
-      if (hitCount === 0) {
-        return {
-          hitCount: 0,
-          source: 'N/A',
-          title: 'N/A',
-          journalTitle: 'N/A',
-          pubYear: item.date,
-          firstPublicationDate: 'N/A',
-          citedByCount: 0,
-        };
-      }
-
-      const publication = resultList.result[0];
-      const {
-        source,
-        title,
-        journalTitle,
-        pubYear,
-        firstPublicationDate,
-        citedByCount,
-      } = publication;
-
-      return {
-        hitCount,
-        source,
-        title,
-        journalTitle,
-        pubYear,
-        firstPublicationDate,
-        citedByCount,
-      };
-    });
-
-    const xAxisCategories = chartData.map(item => item.pubYear);
-    const yAxisCategories = chartData.map((item) => {
-      const {
-        hitCount,
-        source,
-        title,
-        journalTitle,
-        firstPublicationDate,
-        citedByCount,
-      } = item;
-
-      return {
-        y: hitCount,
-        source,
-        title,
-        name: journalTitle,
-        firstPublicationDate,
-        max: citedByCount,
-      };
-    });
-
-    Highcharts.chart('container', {
-      chart: {
-        type: 'column',
-      },
-      title: {
-        text: 'EMBL Publications Statistics',
-      },
-      subtitle: {
-        text: `${term.toUpperCase()} Search: From
-         ${startDate.toDateString()} 
-         To ${endDate.toDateString()}`,
-      },
-      xAxis: {
-        categories: xAxisCategories,
-        crosshair: true,
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Publications',
-        },
-      },
-      tooltip: {
-        headerFormat: '<span style="font-size:10px"><b>{point.y} Publications this year</b></span><table>' +
-          '<p style="font-size:10px">Most Cited Publication</p><table>',
-        pointFormat: '<tr><td style="color:white;padding:0">Journal: </td>' +
-          '<td style="padding:0;color:#999999;"><b>{point.name}</b></td></tr>' +
-
-          '<tr><td style="color:white;padding:0">Title: </td>' +
-          '<td style="padding:0;"><b>{point.title}</b></td></tr>' +
-
-          '<tr><td style="color:white;padding:0">Source: </td>' +
-          '<td style="padding:0";color:#999999;><b>{point.source}</b></td></tr>' +
 
           '<tr><td style="color:white;padding:0">First Published: </td>' +
           '<td style="padding:0"><b>{point.firstPublicationDate}</b></td></tr>' +
